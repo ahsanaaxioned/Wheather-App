4ef40edae9df435341015109af724d59bb4c1ec6
ahsanaaxioned <ahsana@axioned.com>
2022-11-08 13:13:06 +0530

    half js fuctionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 // global variable declaration start here
 const form = document.querySelector(".my-form"),
+  wheatherSection = document.querySelector(".wheather"),
   wrapper = document.querySelector(".wrapper"),
   searchDiv = document.querySelector(".search"),
   input = document.querySelector(".input-content"),
   inputContent = document.querySelector(".input-content"),
-  btn = document.querySelector(".btn"),
+
   heading = document.querySelector(".heading"),
   error = document.querySelector(".error"),
   apiKey = `bdea81d390362a49874c43a13fcf6056`;
@@ -24,9 +25,7 @@ form.addEventListener("click", (e) => {
     error.classList.remove("fail");
     inputContent.classList.remove("fail");
     error.classList.add("success");
-    inputContent.classList.add("success");
     setTimeout(function () {
-      wrapper.classList.add("active")
       getWheather(inputVal);
     }, 500);
   }
@@ -37,7 +36,12 @@ function getWheather(city) {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   fetch(url)
     .then(function (response) {
-      return response.json();
+      // return response.json();
+      if (response.status === 404) {
+        errorApper();
+      } else {
+        return response.json();
+      }
     })
     .then(function (data) {
       // retuning data to show
@@ -46,34 +50,52 @@ function getWheather(city) {
 };
 // API fetch end here
 // function for showing wheather data start here
+
+const dataShow = document.createElement("div");
+dataShow.className = "show-weather";
 function showWheather(data) {
-  const dataShow = document.createElement("div");
-  dataShow.className = "show-weather",
-    dataShow.innerHTML = `<h2 class="wheather-heading">wheather in ${data.name} </h2>
+  if (input.value.toLowerCase() === data.name.toLowerCase()) {
+    dataShow.innerHTML = `<h2 class="wheather-heading">wheather in ${data.name.toLowerCase()} </h2>
   <h3 class="temp">${Math.floor(data.main.temp)} °C</h3> <div class="icon-content">
   <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="wheather Icon" class="icon">
   <span class="description">${data.weather[0].main}</span></div>
   <span class="humidity">humidity: ${data.main.humidity}%</span>
   <span class="wind-speed">wind speed: ${data.wind.speed} km/hour</span>`;
-  searchDiv.appendChild(dataShow);
-}
+    wrapper.appendChild(dataShow);
+    wrapper.classList.add("active");
+    inputContent.classList.add("success");
+  }
+  if (data.main.temp > 30) {
+    console.log(wheatherSection.style.backgroundImage);
+    wheatherSection.style.backgroundImage = 'url("https://www.vmcdn.ca/f/files/sudbury/uploadedImages/SUMMER_sunWater.jpg;w=960/1600x900/")';
+  }
+  if (data.main.temp <= 30) {
+    wheatherSection.style.backgroundImage = 'url("https://images.unsplash.com/photo-1611928482473-7b27d24eab80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80/1600x900/")';
+  }else {
+    errorApper();
+  }
+};
+
+function errorApper() {
+  error.innerText = "*" + input.value + " is not a valid city name";
+  error.classList.remove("success");
+  wrapper.classList.remove("active")
+  error.classList.add("fail");
+  console.log(error);
+  inputContent.classList.add("fail");
+};
 // function for showing wheather data end here
 // event for home page start here
 heading.addEventListener("click", () => {
+  wheatherSection.style.backgroundImage = "";
   wrapper.classList.remove("active");
+  dataShow.remove();
+  error.classList.remove("success");
+  inputContent.classList.remove("success");
 })
 // event for home page end here
 // event for Geolocation start here
-btn.addEventListener("click", () => {
-  if (navigator.geolocation) {
-    // if browser support geolocation API
-    navigator.geolocation.getCurrentPosition(showSuccces, showError);
-    showGeo();
-    wrapper.classList.add(active);
-  } else {
-    alert("sorry your browser dosent support geolocation API")
-  }
-});
+
 function showError() {
   alert("user declined geolocation")
 }
@@ -92,24 +114,35 @@ function fetchGeoapi(geoapi) {
     })
     .then(function (info) {
       // retuning info to show
-       showGeo(info);
+      showGeo(info);
+      const btn = document.querySelector(".btn");
+      btn.addEventListener("click", (e) => {
+        e.preventDefault();
+        if (navigator.geolocation) {
+          // if browser support geolocation API
+          navigator.geolocation.getCurrentPosition(showSuccces, showError);
+          showGeo(info);
+          wrapper.classList.add(active);
+        } else {
+          alert("sorry your browser dosent support geolocation API")
+        }
+      });
     });
 }
 function showGeo(info) {
   const dataShow = document.createElement("div");
   dataShow.className = "show-weather",
-  // console.log(info);
-  dataShow.innerHTML = `<h2 class="wheather-heading">wheather in ${info.name} </h2>
+    // console.log(info);
+    dataShow.innerHTML = `<h2 class="wheather-heading">wheather in ${info.name} </h2>
   <h3 class="temp">${Math.floor(info.main.temp)} °C</h3> <div class="icon-content">
   <img src="https://openweathermap.org/img/wn/${info.weather[0].icon}.png" alt="wheather Icon" class="icon">
   <span class="description">${info.weather[0].main}</span></div>
   <span class="humidity">humidity: ${info.main.humidity}%</span>
   <span class="wind-speed">wind speed: ${info.wind.speed} km/hour</span>`;
   searchDiv.appendChild(dataShow);
+
+
 }
-
-
-// // console.log(result);
 
 
 
