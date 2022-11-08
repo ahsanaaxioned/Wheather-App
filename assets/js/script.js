19af175aed300141ec9e5504b3427d75e5774ab1
ahsanaaxioned <ahsana@axioned.com>
2022-11-08 18:53:33 +0530

    js file added with all functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@
 const form = document.querySelector(".my-form"),
   wheatherSection = document.querySelector(".wheather"),
   wrapper = document.querySelector(".wrapper"),
-  searchDiv = document.querySelector(".search"),
   input = document.querySelector(".input-content"),
   inputContent = document.querySelector(".input-content"),
   heading = document.querySelector(".heading"),
@@ -10,11 +9,11 @@ const form = document.querySelector(".my-form"),
   apiKey = `bdea81d390362a49874c43a13fcf6056`;
 // global variable declaration end here
 
-// form event start here
+// input event start here
 input.addEventListener("keyup", (e) => {
   e.preventDefault();
   const inputVal = input.value;
-  if(e.key == "Enter"){
+  if (e.key == "Enter") {
     if (inputVal === "") {
       error.innerText = "*please enter a city name";
       error.classList.add("fail");
@@ -30,7 +29,13 @@ input.addEventListener("keyup", (e) => {
     }
   }
 });
-// form event end here
+// event for form start here
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+// event for form start here
+
+// input event end here
 // API fetch start here
 function getWheather(city) {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -38,7 +43,7 @@ function getWheather(city) {
     .then(function (response) {
       if (response.status === 404) {
         errorApper();
-      } 
+      }
       if (response.status === 200) {
         return response.json();
       }
@@ -54,8 +59,8 @@ const dataShow = document.createElement("div");
 dataShow.className = "show-weather";
 
 // function for append data for both API start here
-function creatData(appendData){
-dataShow.innerHTML = `<h2 class="wheather-heading">wheather in ${appendData.name.toLowerCase()} </h2>
+function creatData(appendData) {
+  dataShow.innerHTML = `<h2 class="wheather-heading">wheather in ${appendData.name.toLowerCase()} </h2>
 <h3 class="temp">${Math.floor(appendData.main.temp)} °C</h3> <div class="icon-content">
 <img src="https://openweathermap.org/img/wn/${appendData.weather[0].icon}.png" alt="wheather Icon" class="icon">
 <span class="description">${appendData.weather[0].main}</span></div>
@@ -65,27 +70,27 @@ dataShow.innerHTML = `<h2 class="wheather-heading">wheather in ${appendData.name
 // function for append data for both API end here
 
 function showWheather(data) {
-  if(data){
-  if (input.value.toLowerCase() === data.name.toLowerCase()) {
-    const removedataShow = document.querySelector(".show-weather");
-    if(removedataShow){
-      removedataShow.parentElement.removeChild(removedataShow)
+  if (data) {
+    if (input.value.toLowerCase() === data.name.toLowerCase()) {
+      const removedataShow = document.querySelector(".show-weather");
+      if (removedataShow) {
+        removedataShow.parentElement.removeChild(removedataShow)
+      }
+      creatData(data);
+      wrapper.appendChild(dataShow);
+      wrapper.classList.add("active");
+      inputContent.classList.add("success");
+      // changing background according to temp 
+      if (data.main.temp > 30) {
+        wheatherSection.style.backgroundImage = 'url("https://www.vmcdn.ca/f/files/sudbury/uploadedImages/SUMMER_sunWater.jpg;w=960/1600x900/")';
+      }
+      if (data.main.temp <= 30) {
+        wheatherSection.style.backgroundImage = 'url("https://images.unsplash.com/photo-1585088767603-ee684c611b0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80/1600x900/")';
+      }
+    } else {
+      errorApper();
     }
-    creatData(data);
-    wrapper.appendChild(dataShow);
-    wrapper.classList.add("active");
-    inputContent.classList.add("success");
-    // changing background according to temp 
-    if (data.main.temp > 30) {
-      wheatherSection.style.backgroundImage = 'url("https://www.vmcdn.ca/f/files/sudbury/uploadedImages/SUMMER_sunWater.jpg;w=960/1600x900/")';
-    }
-    if (data.main.temp <= 30) {
-      wheatherSection.style.backgroundImage = 'url("https://images.unsplash.com/photo-1585088767603-ee684c611b0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80/1600x900/")';
-    }
-  } else {
-    errorApper();
   }
-}
 };
 
 // function for showing error if response not found
@@ -106,26 +111,27 @@ heading.addEventListener("click", () => {
   inputContent.classList.remove("success");
 })
 // event for home page end here
+
 // event for Geolocation start here
 const btn = document.querySelector(".btn");
-      btn.addEventListener("click", (e) => {
-        e.preventDefault();
-        if (navigator.geolocation) {
-          // if browser support geolocation API
-          navigator.geolocation.getCurrentPosition(showSuccces, showError);
-        } else {
-          alert("sorry your browser dosent support geolocation API")
-        }
-      });
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (navigator.geolocation) {
+    // if browser support geolocation API
+    navigator.geolocation.getCurrentPosition(showSuccces, showError);
+  } else {
+    alert("sorry your browser dosent support geolocation API")
+  }
+});
 function showError() {
   alert("user declined geolocation")
 }
+// Geolocation API fetch start here
 function showSuccces(position) {
   // getting latitude and longitude data from user device
   const { latitude, longitude } = position.coords;
   let geoapi = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
   fetchGeoapi(geoapi);
-  // showWheather(position);
 }
 
 function fetchGeoapi(geoapi) {
@@ -133,7 +139,7 @@ function fetchGeoapi(geoapi) {
     .then(function (response) {
       if (response.status === 404) {
         errorApper();
-      } 
+      }
       if (response.status === 200) {
         return response.json();
       }
@@ -142,10 +148,13 @@ function fetchGeoapi(geoapi) {
       // retuning info to show
       showGeo(info);
     });
-}
+};
+// Geolocation API fetch end here
+
+// showing and append data of Geolocation start here
 function showGeo(info) {
   removedataShow = document.querySelector(".show-weather");
-  if(removedataShow){
+  if (removedataShow) {
     removedataShow.parentElement.removeChild(removedataShow);
   }
   creatData(info);
@@ -153,19 +162,14 @@ function showGeo(info) {
   wrapper.appendChild(dataShow);
   error.classList.remove("fail");
   inputContent.classList.remove("fail");
+  // changing background image according to temp
   if (info.main.temp > 30) {
     wheatherSection.style.backgroundImage = 'url("https://www.vmcdn.ca/f/files/sudbury/uploadedImages/SUMMER_sunWater.jpg;w=960/1600x900/")';
   }
   if (info.main.temp <= 30) {
     wheatherSection.style.backgroundImage = 'url("https://images.unsplash.com/photo-1585088767603-ee684c611b0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80/1600x900/")';
   }
-}
-
-
-
-
-
-
-
+};
+// showing and append data of Geolocation end here
 
 
